4471563151d66d4adf4f53020b289b9679ee48a6
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-10-04 13:41:09 +0300

    clean up files and add first movie component

---
+++ movie-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Movies from './components/Movies';
+
+function App() {
+  return (
+    <div className="App">
+      <Movies />
+    </div>
+  );
+}
+
+export default App;
---
+++ movie-app/src/components/Movies.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Movies() {
+  return (
+    <div>
+        <img className='poster' src='https://m.media-amazon.com/images/M/MV5BMGEzZjdjMGQtZmYzZC00N2I4LThiY2QtNWY5ZmQ3M2ExZmM4XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg' />
+        <h1 className='name'>Scary movie</h1>
+        <p className="comment">A year after disposing of the body of a man they accidentally killed, a group of dumb teenagers are stalked by a bumbling serial killer.</p>
+    </div>
+  )
+}
