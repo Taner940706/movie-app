50f45b715a9036350787764d92d012fb96456ea6
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-10-04 14:10:19 +0300

    working with props

--- movie-app/src/App.tsx
+++ movie-app/src/App.tsx
@@ -6,7 +6,7 @@ import Movies from './components/Movies';
 function App() {
   return (
     <div className="App">
-      <Movies />
+      <Movies items={[{src: 'https://m.media-amazon.com/images/M/MV5BMGEzZjdjMGQtZmYzZC00N2I4LThiY2QtNWY5ZmQ3M2ExZmM4XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg', name: 'Scary Movie', comment: 'A year after disposing of the body of a man they accidentally killed, a group of dumb teenagers are stalked by a bumbling serial killer.'}]}/>
     </div>
   );
 }
--- movie-app/src/components/Movies.tsx
+++ movie-app/src/components/Movies.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 
-export default function Movies() {
+const Movies: React.FC<{items: string[]}> = (props) =>  {
   return (
     <div>
         <img className='poster' src='https://m.media-amazon.com/images/M/MV5BMGEzZjdjMGQtZmYzZC00N2I4LThiY2QtNWY5ZmQ3M2ExZmM4XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg' />
         <h1 className='name'>Scary movie</h1>
         <p className="comment">A year after disposing of the body of a man they accidentally killed, a group of dumb teenagers are stalked by a bumbling serial killer.</p>
+        {props.items.map((item) => (
+            <div className='card'>
+                <img className='poster' src={item.src} />
+                <h1 className='name'>{item.name}</h1>
+                <p className='comment'>{item.comment}</p>
+                </div>
+        ))}
     </div>
   )
 }
+
+export default Movies;
