63190e956dd00fe37e8ae49421ed705be995ff38
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-10-05 11:04:31 +0300

    add ref to the form

--- movie-app/src/components/AddNewMovie.tsx
+++ movie-app/src/components/AddNewMovie.tsx
@@ -1,19 +1,35 @@
+import { useRef } from 'react'
+
 const AddNewMovie = () => {
+
+    const movieNameInputRef = useRef<HTMLInputElement>(null);
+    const moviePosterInputRef = useRef<HTMLInputElement>(null);
+    const movieScoreInputRef = useRef<HTMLInputElement>(null);
+    const movieCommentInputRef = useRef<HTMLInputElement>(null);
 
     const submitHandler = (event: React.FormEvent) =>{
         event.preventDefault;
+
+        const movieName = movieNameInputRef.current!.value;
+        const moviePoster = moviePosterInputRef.current!.value;
+        const movieScore = movieScoreInputRef.current!.value;
+        const movieComment = movieCommentInputRef.current!.value;
+
+        if (movieName.trim().length === 0 && moviePoster.trim().length === 0 && movieScore.trim().length === 0 && movieComment.trim().length === 0){
+            return;
+        }
     }
 
     return (
         <form onSubmit={submitHandler}>
             <label htmlFor="Name: "></label>
-            <input type="text" />
+            <input type="text" ref={movieNameInputRef}/>
             <label htmlFor="Poster: "></label>
-            <input type="text" />
+            <input type="text" ref={moviePosterInputRef}/>
             <label htmlFor="Comment: "></label>
-            <input type="text" />
+            <input type="text" ref={movieCommentInputRef}/>
             <label htmlFor="Score: "></label>
-            <input type="number" />
+            <input type="number" ref={movieScoreInputRef}/>
             <button type="submit">Add Movie</button>
         </form>
     )
