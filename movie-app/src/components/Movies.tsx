b4313859070d044ae8866fea1537884c6e1ad1a2
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-10-10 15:53:40 +0300

    continue to style

--- movie-app/src/components/Movies.tsx
+++ movie-app/src/components/Movies.tsx
@@ -5,13 +5,16 @@ import MovieItem from './MovieItem';
 import classes from './Movies.module.css';
 
 const Movies: React.FC<{items: Movie[], onRemoveMovie: (id: string) => void}> = (props) =>  {
-  return (
+  return <>
     <div className={classes.main}>
         {props.items.map((item) => (
             <MovieItem onRemoveMovie={props.onRemoveMovie.bind(null, item.id)} key={item.id} movie={item} />
         ))}
     </div>
-  )
+    <div className={classes.error}>
+  {props.items.length===0 && <p className={classes['error-text']}>Empty list!</p>}
+  </div>
+  </>
 }
 
 export default Movies;
