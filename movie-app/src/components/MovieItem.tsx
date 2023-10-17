1dcc465d6f5d17d1e3ed1e3630b1331b1d8f5904
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-10-17 20:49:16 +0300

    fix css module

--- movie-app/src/components/MovieItem.tsx
+++ movie-app/src/components/MovieItem.tsx
@@ -5,13 +5,13 @@ const MovieItem: React.FC<{movie: Movie, onRemoveMovie: () => void}> = (props) =
     return (
         <>
         <div id={props.movie.id} className={classes.card}>
-                <h1 className='name'>{props.movie.name}</h1>
-                <img className='poster' src={props.movie.src} />
-                <p className='comment'>Comment: {props.movie.comment}</p>
-                <p className='score'>Score: {props.movie.score}</p>
-                <p className='date'>Date: {props.movie.date}</p>
-                <p className='genre'>Genre: {props.movie.genre}</p>
-                <p className='duration'>Duration: {props.movie.duration}</p>
+                <h1 className={classes.name}>{props.movie.name}</h1>
+                <img className={classes.poster} src={props.movie.src} />
+                <p className={classes.comment}>Comment: {props.movie.comment}</p>
+                <p className={classes.score}>Score: {props.movie.score}</p>
+                <p className={classes.date}>Date: {props.movie.date}</p>
+                <p className={classes.genre}>Genre: {props.movie.genre}</p>
+                <p className={classes.duration}>Duration: {props.movie.duration}</p>
                 <button onClick={props.onRemoveMovie}>Delete</button>
                 </div>
                 </>
